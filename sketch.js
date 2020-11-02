c39cb6b42a510189d15fc4dfd58eed81c7715409
sakshiharjani <71299642+sakshiharjani@users.noreply.github.com>
2020-11-01 21:34:21 -0500

    updated with buttons and styles of dom elements; cant resize properly

--- sketch.js
+++ sketch.js
@@ -2,6 +2,11 @@ let inp;
 let radio;
 let slider;
 let sel;
+let button1;
+let button2;
+let button3;
+let button4;
+let img;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(200, 100, 200)
@@ -9,6 +14,7 @@ function setup() {
   
   inp = createInput('');
   inp.input(Name);
+  inp.id('unique-input')
   
   inp.size(250, 30);
   
@@ -17,27 +23,45 @@ function setup() {
   radio = createRadio();
   radio.option('Yes');
   radio.option('No');
-  radio.position(350, 270)
+  
   radio.style('width', '60px');
   textAlign(CENTER);
   fill(255, 0, 0);
   
+  repositionRadio();
+  
   slider = createSlider(0, 200, 100, 100);
-  slider.position(300, 415);
   slider.style('width', '200px');
+  
+  repositionSlider();
 
   textAlign(CENTER);
   background(200, 200, 100);
   sel = createSelect();
-  sel.position(400, 545);
   sel.option('(choose)');
   sel.option('mail');
   sel.option('in person');
   sel.changed(mySelectAnswer);
   sel.size(250, 20)
   
+  repositionSelect();
   
- 
+  button1 = createButton('Enter');
+  button2 = createButton('Enter');
+  button3 = createButton('Enter');
+  button4 = createButton('Enter');
+  button1.class('group-button')
+  button2.class('group-button')
+  button3.class('group-button')
+  button4.class('group-button')
+  
+  button1.mousePressed(checkMark1);
+  button2.mousePressed(checkMark2);
+  button3.mousePressed(checkMark3);
+  button4.mousePressed(checkMark4);
+  
+  repositionButtons();
+    
 
 }
 
@@ -50,6 +74,43 @@ function mySelectAnswer(){
   background(100, 200, 100);
 }
 
+function checkMark1() {
+  
+//image source: <a href="https://www.freeiconspng.com/img/5385">Green Check Mark</a>
+  img = createImg("https://www.freeiconspng.com/uploads/green-check-mark-16.jpg", '')
+  
+  img.size(50, 50)
+
+  repositionImg1();
+}
+function checkMark2() {
+  
+//image source: <a href="https://www.freeiconspng.com/img/5385">Green Check Mark</a>
+  img = createImg("https://www.freeiconspng.com/uploads/green-check-mark-16.jpg", '')
+  
+  img.size(50, 50)
+
+  repositionImg2();
+}
+function checkMark3() {
+  
+//image source: <a href="https://www.freeiconspng.com/img/5385">Green Check Mark</a>
+  img = createImg("https://www.freeiconspng.com/uploads/green-check-mark-16.jpg", '')
+  
+  img.size(50, 50)
+
+  repositionImg3();
+}
+function checkMark4() {
+  
+//image source: <a href="https://www.freeiconspng.com/img/5385">Green Check Mark</a>
+  img = createImg("https://www.freeiconspng.com/uploads/green-check-mark-16.jpg", '')
+  
+  img.size(50, 50)
+
+  repositionImg4();
+}
+
 function draw() {
   background(200, 100, 200)  
   
@@ -57,6 +118,7 @@ function draw() {
   fill(0, 0, 0)
   textSize(48);
   textAlign(CENTER);
+  textFont('Georgia')
   text('Voting Questionnaire', windowWidth/2 + 50, windowHeight/2 - 300)
   
   fill(0, 0, 0)
@@ -74,9 +136,9 @@ function draw() {
   textAlign(CENTER);
   text('Have you voted?', 150, 430)
   fill(200, 0, 0)
-  text('No', 500, 455)
+  text('No', 375, 515)
   fill(0, 200, 0)
-  text('Yes', 300, 455)
+  text('Yes', 175, 515)
   
   fill(0, 0, 0)
   textSize(24);
@@ -84,13 +146,60 @@ function draw() {
   text('Did you vote by mail or in person?', 200, 560)
 }
 
-function repositionInput() {
-  inp.position(290, 150, 170);
+
+
+function repositionButtons() {
+  button1.position(windowWidth/2 + 10, windowHeight/2 - 160)
+  button2.position(windowWidth/2 - 40, windowHeight/2 - 40)
+  button3.position(windowWidth/2 - 20, windowHeight/2 + 90)
+  button4.position(windowWidth/2 + 30, windowHeight/2 + 230)
   
 }
+
+function repositionInput() {
+  inp.position(windowWidth/2 - 300, windowHeight/2 - 150);
+  
+}
+
+function repositionRadio() {
+  radio.position(windowWidth/2 - 190, windowHeight/2 - 30)
+}
+
+function repositionSlider() {
+  slider.position(windowWidth/2 - 280, windowHeight/2 + 130);
+}
+
+function repositionSelect() {
+  sel.position(windowWidth/2 - 250, windowHeight/2 + 250);
+}
+
+function repositionImg1() {
+  img.position(windowWidth/2 + 150, windowHeight/2 - 160)
+}
+function repositionImg2() {
+  img.position(windowWidth/2 + 150, windowHeight/2 - 40)
+}
+
+function repositionImg3() {
+  img.position(windowWidth/2 + 100, windowHeight/2 + 90)
+}
+
+function repositionImg4() {
+  img.position(windowWidth/2 + 150, windowHeight/2 + 230)
+}
+
+
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   repositionInput();
+  repositionRadio();
+  repositionButtons();
+  repositionSlider();
+  repositionSelect();
+  repositionImg1();
+  repositionImg2();
+  repositionImg3();
+  repositionImg4();
   
 }
